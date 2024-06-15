3ccc26d839b3ff67a0181d281d8ad39249ab30dd
jiyun <ranji12@naver.com>
2024-06-15 14:22:51 +0900

    객체 만드는 함수 모듈

---
+++ restAPI/objFunc.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @param {객체함수의 번호 값} id 
+ * @param {객체함수의 제목 값} title 
+ * @param {객체함수의 내용 값} body 
+ * @returns 
+ */
+const func = (id, title, body) => {
+    return {
+        번호: id,
+        제목: title,
+        내용: body
+    }
+}
