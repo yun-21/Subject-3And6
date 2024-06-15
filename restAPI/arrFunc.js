22b6d9921d654d27446b4ef0bbb3ba7c1ea944b0
jiyun <ranji12@naver.com>
2024-06-15 17:50:40 +0900

    parse된 데이터 반복문으로 각각의 배열에 넣는 함수 모듈

---
+++ restAPI/arrFunc.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @param {data} data forEach문 돌릴 매개변수 
+ * @param {array} array 새로운 배열 매개변수 
+ * @param {num} num 반복문으로 돌릴 인덱스번호 
+ */
+const arrPush = (data, array, num) => {
+    data.forEach((element) => {
+        array.push(element[num])
+    })
+}
+const allArr = {
+    id: idArr = [],
+    title: titleArr = [],
+    content: contentArr = []
+}
